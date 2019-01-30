21f8909aaf2f345fd1963c88a02de25b4af4499a
Chris <chrisbeckley001@gmail.com>
2019-01-30 17:56:44 -0500

    helper js update

---
+++ scripts/helper.js
@@ -0,0 +1,13 @@
+class Helper {
+
+
+playPauseAndUpdate (song) {
+  player.playPause(song);
+
+  const duration = player.currentlyPlaying.duration;
+  $('#time-control .total-time').text(duration);
+
+}
+
+}
+ const helper = new Helper();
