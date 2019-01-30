0054d6b63f71c850b52069ca9fd2c9f27344ca0f
Chris <chrisbeckley001@gmail.com>
2019-01-30 16:24:51 -0500

    added song-list and player-bar js updates

---
+++ scripts/song-list.js
@@ -0,0 +1,22 @@
+{
+   album.songs.forEach( (song, index) => {
+   song.element = $(`
+        <tr>
+        <td>
+       <button>
+         <span class="song-number">${index + 1}</span>
+         <span class="ion-play"></span>
+         <span class="ion-pause"></span>
+       </button>
+     </td>
+         </tr>
+      `);
+
+      song.element.on('click', event => {
+       player.playPause(song);
+      $('button#play-pause').attr('playState', player.playState);
+     });
+
+  $('#song-list').append(song.element);
+});
+ }
